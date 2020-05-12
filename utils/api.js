8fa06f06ee0333c23d9454229005d2bad90ab410
Asmaa Abou Bakr <asmaa.abobaker@yahoo.com>
2020-05-12 20:29:54 +0200

    add redux store

--- utils/api.js
+++ utils/api.js
@@ -1,3 +1,7 @@
+import { AsyncStorage } from 'react-native'
+
+const FLASHCARDS_STORAGE_KEY = 'flashcards: decks'
+
 const initialData = {
     React: {
       title: 'React',
@@ -25,4 +29,26 @@ const initialData = {
 
   export const getData = () => {
       return initialData
+  }
+
+  export function getDecks (deck) {
+    return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY)
+    .then(results => {
+      if(results === null) {
+        AsyncStorage.setItem(FLASHCARDS_STORAGE_KEY, JSON.stringify(initialData))
+        return initialData
+      }else {
+        return JSON.parse(results)
+      }
+    })
+  }
+
+
+export function saveDeckTitle(title){
+    return AsyncStorage.mergeItem(FLASHCARDS_STORAGE_KEY, JSON.stringify({
+      [title]: {
+        title: title,
+        questions: []
+      }
+    }))
   }
