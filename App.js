d7af21b5313f959b8598e1ff778ff444c998ec94
Asmaa Abou Bakr <asmaa.abobaker@yahoo.com>
2020-05-13 02:24:11 +0200

    create bottom tabs navigation

--- App.js
+++ App.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DeckList from './components/DeckList';
+import AddDeck from './components/AddDeck'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
-import AdddDeck from './components/AddDeck';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import {FontAwesome, MaterialCommunityIcons} from '@expo/vector-icons'
+import { purple, white } from './utils/color'
+
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
-      <View style={styles.container}>
-        <DeckList/>
-        <AdddDeck/>
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="DeckList" component={DeckList} />
+            <Tab.Screen name="AddDeck" component={AddDeck} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </View>
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
